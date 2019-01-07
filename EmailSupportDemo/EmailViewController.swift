31783f9c3b636a94830e782c9ee4f491d746c5cb
George Garcia <geor@vicki-3.local>
2019-01-07 12:26:15 -0800

    - First commit

--- EmailSupportDemo/EmailViewController.swift
+++ EmailSupportDemo/EmailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EmailViewController.swift
 //  EmailSupportDemo
 //
 //  Created by George Garcia on 1/7/19.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EmailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
